ff4c0845ff36a9e65fba999f6ab1ec958de1ac32
Pravin Kandala <pravinkandala@hotmail.com>
2016-05-03 08:26:20 -0500

    updated strings.xml and State.java file

---
+++ app/src/main/java/edu/niu/cs/z1761257/jsonexample/State.java
@@ -0,0 +1,17 @@
+package edu.niu.cs.z1761257.jsonexample;
+
+/**
+ * Created by Pravin on 5/3/16.
+ */
+public class State
+{
+    public final String stateAbbr;
+    public final int stateNumber;
+
+    public State( String newAbbr, int newNumber )
+    {
+        stateAbbr = newAbbr;
+        stateNumber = newNumber;
+    }
+}//end State class
+
